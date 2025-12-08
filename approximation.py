5629633ae4b47700dbeaecb924e9d32a3e5cacf4
dd-gc <194241605+dd-gc@users.noreply.github.com>
2025-12-08 13:56:59 -0500

    Updated to compute key points in passband.

--- approximation.py
+++ approximation.py
@@ -4,26 +4,42 @@ from matplotlib import pylab as plt
 
 
 if __name__ == '__main__':
-  
+
     if False:
-        x = np.linspace(0.0, 2.0)
-        N = 3
-        u = np.arccos(x.astype(complex))
-        y = np.cos(N * u)
+        w = np.linspace(0.0, 2.0, num=1000)
+        N = 5
+        u = np.arccos(w.astype(complex))  # real in passband, imag in cutoff
+        y = np.cos(N * u)  # purely real, except for roundoff
+        k1 = 0.1  # arbitrary plotting kludge
+        # compute key points
+        vals = [1, 0, -1, 0]
+        key_freqs = [np.cos(U/N*np.pi/2) for U in reversed(range(N+1))]
+        key_vals  = [vals[U%4] for U in reversed(range(N+1))]
+
     else:
-        x = np.linspace(0.0, 4.0, num=1000)
-        k = 1 / 1.1
-        N = 3
+        w = np.linspace(0.0, 4.0, num=1000)
+        k = 1 / 1.05
+        N = 5
         L = N//2
         ui = np.array([(2*j+1)/N for j in range(L)])  # i = j + 1
+        K = ef.complete_elliptic_integral(k)
         k1 = k**N * np.prod(ef.sne(ui,k)**4)
-        K  = ef.complete_elliptic_integral(k)
         K1 = ef.complete_elliptic_integral(k1)
-        u = ef.acde(x.astype(complex),k)
-        y = ef.cde(N*u,k1)
-        
+        u = ef.acde(w.astype(complex),k)  # real C->S, imag S->N, complex N->D
+        y = ef.cde(N*u,k1)  # purely real, except for roundoff
+
+        # We only compute the passband key points, because the stopband key
+        # points can be computed from the passband points, and because the
+        # stopband contains NaN values that correspond to +/-Inf points.
+        vals = [1, 0, -1, 0]
+        key_freqs = [ef.cde(U/N,k) for U in reversed(range(N+1))]
+        key_vals  = [vals[U%4] for U in reversed(range(N+1))]
+
     ep2 = 0.5
 
-    plt.plot(x, np.sqrt(1 / (1 + ep2 * np.real(y)**2)))
+    # plt.plot(x, np.sqrt(1 / (1 + ep2 * np.real(y)**2)))
+    plt.plot(w, np.real(y), '-')
+    plt.plot(np.real(key_freqs), np.real(key_vals), 'o')
+    plt.ylim(np.array([-1,1])/k1*1.5)
     plt.grid(True)
     plt.show()
